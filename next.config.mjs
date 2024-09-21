f69efa022c134092d34a1325dd3324582916d8e8
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-09-21 23:48:26 +0300

    package.json update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  compilerOptions: {
+    baseUrl: '.',
+    paths: {
+      '@/*': ['./*'],
+    },
+  },
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    formats: ['image/avif', 'image/webp'],
+  },
+};
 
 export default nextConfig;
